adfd06a2613d66470c6571b555c3469c03cdb55d
Jairo <camiloacostajairo5@gmal.com>
2025-01-29 20:19:52 -0400

    Agregando hilt

--- app/src/main/java/ucne/edu/jairocamilo_p1_ap2/MainActivity.kt
+++ app/src/main/java/ucne/edu/jairocamilo_p1_ap2/MainActivity.kt
@@ -11,37 +11,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import ucne.edu.jairocamilo_p1_ap2.ui.theme.JairoCamilo_P1_AP2Theme
+import androidx.room.Room
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             JairoCamilo_P1_AP2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JairoCamilo_P1_AP2Theme {
-        Greeting("Android")
-    }
-}
