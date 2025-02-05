f4becb9c05e34c3a35e2c86ba111c8aec47ac88e
Jairo <camiloacostajairo5@gmal.com>
2025-02-05 18:33:42 -0400

    Agregando la entidad de sistema.

--- app/src/main/java/ucne/edu/jairocamilo_p1_ap2/data/local/entities/SistemasEntity.kt
+++ app/src/main/java/ucne/edu/jairocamilo_p1_ap2/data/local/entities/SistemasEntity.kt
@@ -3,8 +3,9 @@ package ucne.edu.jairocamilo_p1_ap2.data.local.entities
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity("Entity")
-data class Entity(
+@Entity(tableName = "Sistemas")
+data class SistemasEntity(
     @PrimaryKey
-    val entityId: Int? = null
+    val sistemaId: Int? = null,
+    val nombre: String = ""
 )
